a1e16b7a5d39db62edbda2be21299bbf20724f50
JoaoVitorOliMendes <joao.vitor@headot.com>
2021-08-06 17:52:41 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+function log() {
+  console.log("Button clicked");
+}
+
+const Node1 = (props) => <button onClick={log}> { props.text } </button>
+const Node2 = () => <h1>Elemento 1</h1>
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Node2 />
+      <h1>Titulo</h1>
+      <Node1 text="Click" />
     </div>
   );
 }
