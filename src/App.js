fb6ab69fb5d34f8a0c1f386f0bc21e994200dffd
JoaoVitorOliMendes <joao.vitor@headot.com>
2021-08-11 11:22:33 -0300

    exercicio2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
-function log() {
-  console.log("Button clicked");
+const Nome = (props) => {
+  return <p><b>{props.nome}</b></p>
 }
 
-const Node1 = (props) => <button onClick={log}> { props.text } </button>
-const Node2 = () => <h1>Elemento 1</h1>
+const list = [
+  "banana",
+  "maca",
+  "pera",
+  "uva"
+]
+
+const Lista = () => {
+  return list.map((val, i)=>{
+    return <li key={i}>
+      <Button onClick={() => log(val)} variant="success">
+       {val}
+      </Button>
+    </li>
+  })
+}
+
+const Image = () => {
+  return <img 
+  src="https://www.dailydot.com/wp-content/uploads/2019/02/watch_family_guy_season_17_online_free_amazon.jpg"
+  alt="familyGuy"></img>
+}
+
+const Link = () => {
+  return <a href="https://google.com">Google</a>
+}
 
 
 function App() {
   return (
     <div className="App">
-      <Node2 />
-      <h1>Titulo</h1>
-      <Node1 text="Click" />
+      <Nome nome="João Vitor de Oliveira Mendes"></Nome>
+     <ul>
+       <Lista></Lista>
+     </ul>
+       <Image></Image>
+       <br />
+        <Button variant="danger">
+          <Link></Link>
+        </Button>
     </div>
   );
+}
+
+function log(val) {
+  alert(val)
 }
 
 export default App;
